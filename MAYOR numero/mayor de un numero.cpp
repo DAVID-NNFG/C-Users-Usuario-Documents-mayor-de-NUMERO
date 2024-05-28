ecc281e6ab43c0945e46cc2a8293fcf83a311b5c
victor.macas <victor.macas@unl.edu.ec>
2024-05-27 22:52:01 -0500

    ejercisios de mayor

---
+++ MAYOR numero/mayor de un numero.cpp
@@ -0,0 +1,52 @@
+#include <studio> 
+
+int main() {
+ 
+    int n1,n2,n3,n4,n5
+   cout<<"ingrese el numero a"; 
+   cin>>n1; 
+   cout<<"ingrese el numero b";
+   cin>>n2;
+   cout<<"ingrese el numero c";
+   cin>>n3;
+   cout<<"ingrese el numero d";
+   cin>>n4;
+   cout<<"ingrese el numero f";
+   cin>>n5;
+
+   if(n1>n2 && n1>n3 && n1>n4 && n1>n5){
+
+    cout <<"el mayor es" <<n1;
+   }
+   else{
+
+   if (n2>n1 && n2>n3 && n2>n4 && n2>n5){
+
+    cout <<"el mayor es" <<n2;
+   }
+   else{
+
+   if (n3>n1 && n3>n2 && n3>n4 && n3>n5){
+    
+    cout <<"el mayor es" <<n3; 
+  }
+   else{
+
+   if(n4>n1 && n4>n2 && n4>n3 && n4>n5){
+
+    cout <<"el numero mayor es" <<n4;
+   } 
+   else{
+   
+   if(n5>n1 && n5>n2 && n5>n3 && n5>n4){
+
+    cout <<"el numero mayor es"  <<n5;
+   }
+   else{
+   
+    
+ return 0     
+}
+
+ 
+
